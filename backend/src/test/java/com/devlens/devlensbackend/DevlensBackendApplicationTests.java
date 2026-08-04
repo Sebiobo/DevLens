a6da8ab3f78221eb021c78715f5427c6f69106d6
Sebiobo <oboroceanusebi@yahoo.com>
2026-08-04 15:34:37 +0300

    feat: initialize Spring Boot backend

---
+++ backend/src/test/java/com/devlens/devlensbackend/DevlensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlens.devlensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
